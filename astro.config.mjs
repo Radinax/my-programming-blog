7e98750ccbcb9ede1f722d3aab5aff16491de4d0
Adrian <adrberia@gmail.com>
2025-08-13 23:45:54 -0400

    Test deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,16 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
-import expressiveCode from "astro-expressive-code";
 import tailwind from "@astrojs/tailwind";
-
-// Import rehype plugins
-import rehypeMermaid from "rehype-mermaid";
+import remarkMermaid from "remark-mermaidjs";
 
 export default defineConfig({
   site: "https://adrian-beria-blog.netlify.app/",
   markdown: {
-    remarkPlugins: [remarkToc],
-    rehypePlugins: [rehypeMermaid],
-    shikiConfig: { theme: "css-variables" },
+    remarkPlugins: [remarkToc, remarkMermaid],
+    rehypePlugins: [],
+    syntaxHighlight: "shiki",
+    shikiConfig: { theme: "css-variables", wrap: true },
   },
-  integrations: [
-    expressiveCode(),
-    mdx({ syntaxHighlight: "shiki" }),
-    sitemap(),
-    tailwind(),
-  ],
+  integrations: [mdx({ syntaxHighlight: "shiki" }), sitemap(), tailwind()],
 });
